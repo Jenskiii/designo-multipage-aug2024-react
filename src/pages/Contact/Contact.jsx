6162a7d7d88e30e420e680b16aa228b77a97be27
Jens Kapteijn <124568361+Jenskiii@users.noreply.github.com>
2024-09-02 10:28:18 +0100

    added leaf to contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,47 +7,45 @@ import styles from "./Contact.module.css";
 export function Contact() {
   return (
     <>
-      {/* HERO */}
-      <section
-        className="container"
-        aria-label="full-bleed"
-        aria-labelledby="contact-hero">
-        {/* HERO BODY*/}
-        <div className={`${styles.hero} | hero`}>
-          <div className={styles["hero-body"]}>
-            <h1 id="contact-hero" className="heading-1">
-              {data.hero.title}
-            </h1>
-            <p>{data.hero.paragraph}</p>
-
-            {/* HERO BG PATTERN */}
-            <picture className={styles["hero-pattern"]}>
-              <source
-                media="(min-width: 46.5rem)"
-                srcSet={data.hero.bg_pattern.desktop}
-              />
-              <img
-                src={data.hero.bg_pattern.mobile}
-                alt="Multiple people around a table working on their laptop"
-              />
-            </picture>
+      <main>
+        {/* HERO */}
+        <section
+          className="container"
+          aria-label="full-bleed"
+          aria-labelledby="contact-hero">
+          {/* HERO BODY*/}
+          <div className={`${styles.hero} | hero`}>
+            <div className={styles["hero-body"]}>
+              <h1 id="contact-hero" className="heading-1">
+                {data.hero.title}
+              </h1>
+              <p>{data.hero.paragraph}</p>
+              {/* HERO BG PATTERN */}
+              <picture className={styles["hero-pattern"]}>
+                <source
+                  media="(min-width: 46.5rem)"
+                  srcSet={data.hero.bg_pattern.desktop}
+                />
+                <img
+                  src={data.hero.bg_pattern.mobile}
+                  alt="Multiple people around a table working on their laptop"
+                />
+              </picture>
+            </div>
+            {/* FORM */}
+            <Form>
+              <FormGroup label="Name" id="name" />
+              <FormGroup label="Email Address" id="email" />
+              <FormGroup label="Phone" id="phone" />
+              <FormTextArea label="Your Message" id="textarea" />
+            </Form>
           </div>
-
-          {/* FORM */}
-          <Form>
-            <FormGroup label="Name" id="name" />
-            <FormGroup label="Email Address" id="email" />
-            <FormGroup label="Phone" id="phone" />
-            <FormTextArea label="Your Message" id="textarea" />
-          </Form>
-        </div>
-      </section>
-
-      {/* LINKS */}
-      <LocationLinks className={styles.locations} />
-
-      {/* LEAF */}
-      <Leaf className={styles.leaf}/>
+        </section>
+        {/* LINKS */}
+        <LocationLinks className={styles.locations} />
+        {/* LEAF */}
+        <Leaf className={styles.leaf} />
+      </main>
     </>
   );
 }
